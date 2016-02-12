0ada93a521f6e36927f12a94e27d9c61aca7c68f
David Zukowski <zukoweb@gmail.com>
2016-02-12 12:30:15 -0500

    upgrade(react-router-redux): ^3.0.0 -> ^4.0.0-beta

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { browserHistory } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
 import makeRoutes from './routes'
 import Root from './containers/Root'
 import configureStore from './redux/configureStore'
@@ -11,9 +12,19 @@ import it from 'react-intl/lib/locale-data/it'
 import es from 'react-intl/lib/locale-data/es'
 import fr from 'react-intl/lib/locale-data/fr'
 
+// Create redux store and sync with react-router-redux. We have installed the
+// react-router-redux reducer under the key "router" in src/routes/index.js,
+// so we need to provide a custom `selectLocationState` to inform
+// react-router-redux of its location.
 const initialState = window.__INITIAL_STATE__
-const store = configureStore({ initialState, history })
+const store = configureStore(initialState)
+const history = syncHistoryWithStore(browserHistory, store, {
+  selectLocationState: (state) => state.router
+})
 
+// Now that we have the Redux store, we can create our routes. We provide
+// the store to the route definitions so that routes have access to it for
+// hooks such as `onEnter`.
 const routes = makeRoutes(store)
 
 addLocaleData(en)
@@ -41,8 +52,10 @@ function start () {
   )
 }
 
-// Render the React application to the DOM
+// Now that redux and react-router have been configured, we can render the
+// React application to the DOM!
+
 ReactDOM.render(
-  <Root history={browserHistory} routes={routes} store={store} />,
+  <Root history={history} routes={routes} store={store} />,
   document.getElementById('root')
 )

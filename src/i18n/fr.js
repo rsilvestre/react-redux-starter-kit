366fa250dda8817401f6a76513492ceea05b347d
Michael Silvestre <willtard@gmail.com>
2016-04-05 23:27:04 +0200

    fix(translation): Change text in french

--- src/i18n/fr.js
+++ src/i18n/fr.js
@@ -1,7 +1,7 @@
 /*eslint-disable max-len,quotes*/
 export default {
   "home.welcome": "Bienvenue au Starter Kit du React Redux",
-  "home.sampleCounter": "Contateur d'example:",
+  "home.sampleCounter": "Exemple de compteur:",
   "languageSelector.spanish": "Español",
   "languageSelector.english": "Inglés",
   "languageSelector.french": "Français"
